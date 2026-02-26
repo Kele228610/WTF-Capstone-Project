61613ef238b55a43ae964710a80e4aecd0cfc81e
Kele228610 <lebididik67@gmail.com>
2026-02-26 13:59:37 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,46 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   esbuild: {
-//     loader: 'jsx',
-//     include: /src\/.*\.(js|jsx)$/,
-//   },
-// })
-
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      injectRegister: "auto",
+      includeAssets: [
+        "favicon.ico",
+      ],
+
       manifest: {
         name: "EduLearn Offline",
         short_name: "EduLearn",
         description: "Offline-first learning platform",
         theme_color: "#124CC9",
-        background_color: "#ffffff",
+        background_color: "#124CC9",
         display: "standalone",
         start_url: "/",
+        scope: "/",
+
         icons: [
-          { src: "/pwa-192x192.png", sizes: "192x192", type: "image/png" },
-          // { src: "Edulearn logo.png", sizes: ""}
-          { src: "/pwa-512x512.png", sizes: "512x512", type: "image/png" },
-          { src: "/pwa-512x512.png", sizes: "512x512", type: "image/png", purpose: "any maskable" }
+          {
+            src: "/edulearn-logo-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/edulearn-logo-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable"
+          }
         ]
       },
+
       workbox: {
         navigateFallback: "/index.html",
+        cleanupOutdatedCaches: true
       }
     })
   ]
